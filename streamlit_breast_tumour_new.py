e96d3f8dc8dd6833ec26e9d8ebd6a1a80eedcafb
Brindha Ganesan <50005288+brindhaganesan@users.noreply.github.com>
2024-06-10 19:40:58 -0700

    Update streamlit_breast_tumour_new.py

--- streamlit_breast_tumour_new.py
+++ streamlit_breast_tumour_new.py
@@ -47,7 +47,7 @@ def load_model() -> tf.keras.Model:
     Returns:
         tf.keras.Model: Trained convolutional neural network.
     """
-    return tf.keras.models.load_model('custom_cnn_model.keras')
+    return tf.keras.models.load_model('custom_cnn_new.h5')
 
 
 def get_prediction(image: Image.Image | tf.Tensor) -> None:
